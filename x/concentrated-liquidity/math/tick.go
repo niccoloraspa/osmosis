0c5f3b89fbd0297a34cb95fe3bf7e6d657339002
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-09-07 14:30:22 +0300

    use osmomath.PrecisionDec

--- x/concentrated-liquidity/math/tick.go
+++ x/concentrated-liquidity/math/tick.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/osmosis-labs/osmosis/osmomath"
 	"github.com/osmosis-labs/osmosis/v19/x/concentrated-liquidity/types"
 )
